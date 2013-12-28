16557bb92cb2f42acfbe45aa72a26c1a2eefe2f2
Olivier Scherrer <pode.fr@gmail.com>
2013-12-28 16:00:06 -0500

    missing file

---
+++ quick-dom.js
@@ -0,0 +1,12 @@
+/**
+ * quick-dom.js - http://podefr.github.com/quick-dom/
+ * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
+"use strict";
+
+var jsdom = require("jsdom").jsdom;
+GLOBAL.document = jsdom("<html><head></head><body></body></html>");
+GLOBAL.window = document.parentWindow;
+
+module.exports = jsdom;
